96e8f708bc1184f1ffded878dd3a971376688011
GitHub Action <action@github.com>
2021-11-27 06:07:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/dparnell/intellijwgsl/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dparnell/intellijwgsl/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dparnell.intellijwgsl.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dparnell.intellijwgsl.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/dparnell/intellijwgsl/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dparnell/intellijwgsl/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dparnell.intellijwgsl.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dparnell.intellijwgsl.MyBundle
 
 class MyProjectService(project: Project) {
 
