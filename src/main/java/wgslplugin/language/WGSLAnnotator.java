8d177765e2495296d87f12a71f4f84cc270ed17b
Daniel Parnell <me@danielparnell.com>
2021-11-28 21:44:56 +1100

    published

--- src/main/java/wgslplugin/language/WGSLAnnotator.java
+++ src/main/java/wgslplugin/language/WGSLAnnotator.java
@@ -4,7 +4,6 @@ import com.intellij.lang.ASTNode;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
